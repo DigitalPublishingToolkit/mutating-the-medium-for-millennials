2b4a82a9208fdd13010e70e935838b75975e1770
Lucia Dossin <lucia@fiveblackcats.com>
2016-05-19 15:27:01 +0200

    moving slider snippet - rotates when dragged (not tested on touch devices yet, not connected to image gallery yet)

---
+++ js/rotating-slider.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+var thumbPos, origin, originPerc, degToRotate;
+
+var scale = d3.scale.linear()
+                    .domain([0, 25, 50, 75, 100])
+                    .range([-4, -2, 0, 2, 4]);
+
+$("#slider").on("input change", function(){
+  thumbPos = ((this.value - this.min / this.max - this.min) / this.max) * $(this).width();
+  origin = thumbPos + 10;
+  originPerc = origin / $(this).width() * 100;
+  //$('output').text( thumbPos );
+  //$('#cross').css('left', origin); // 10 is half of the thumb width, defined in CSS
+
+  degToRotate = scale(originPerc);
+  //console.log(originPerc, degToRotate);
+
+  $(this).css({
+     'transform': 'rotate('+degToRotate+'deg)',
+     'transform-origin': originPerc + '% 0%'
+  });
+
+});
+
+
+});
