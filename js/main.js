73d22fb00251ffbe649235b5adec95955b816e6d
Nadine <nadine@rotem.eu>
2016-05-23 14:39:08 +0200

    added new article pages and js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+$(document).ready(function(){
 
+	//code
+	$('.search-bar').on("click", function(){
+		$('#search-form').toggle();
+	});
+
+	var links=[
+	
+	"articleprison.html",
+	"articlegreenery.html",
+	"articleghanaian.html",
+	"articledisorders.html",
+	"articleconflict.html",
+	"articlecook.html"
+	
+
+	];
+
+	for (var i=0; i<links.length; i++){
+		//append link to article page
+		$('.small-article-heading').eq(i).parent('.col-sm-3').wrap('<a href="'+links[i]+'"></a>');
+	}
+	
+
+});
